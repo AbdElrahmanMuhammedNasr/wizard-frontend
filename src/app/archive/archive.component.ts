d417bbef365a96d6ba6f5db50f198f5a2b67810a
AbdElrahmanMuhammedNasr <nasr.abdelrahmanmohamed@gmail.com>
2021-03-13 09:44:23 +0200

    update

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -19,12 +19,49 @@ export class ArchiveComponent implements OnInit {
     }, {
       number: 6,
       image: '../../assets/1.jpeg'
-    },  {
+    }, {
+      number: 6,
+      image: '../../assets/1.jpeg'
+    },
+    {
+      number: 1,
+      image: '../../assets/1.jpeg'
+    }, {
+      number: 2,
+      image: '../../assets/1.jpeg'
+    }, {
+      number: 6,
+      image: '../../assets/1.jpeg'
+    }, {
+      number: 6,
+      image: '../../assets/1.jpeg'
+    }, {
       number: 6,
       image: '../../assets/1.jpeg'
     },
 
+  ];
 
+  statistic = [
+    {
+      name: 'Total Ads',
+      number: 12
+    }, {
+      name: 'Total views',
+      number: 12
+    }, {
+      name: 'Total Action',
+      number: 12
+    }, {
+      name: 'Facebook Ads',
+      number: 12
+    }, {
+      name: 'Instagram Ads',
+      number: 12
+    }, {
+      name: 'Twitter Ads',
+      number: 12
+    },
   ];
 
   constructor() {
